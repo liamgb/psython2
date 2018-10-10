5bd056999a808517fe4edb6a0edbfeef347a02fb
humfy <moc.liamgb@gmail.com>
2018-10-10 17:09:16 -0600

    configuration

---
+++ README.md
@@ -0,0 +1,2 @@
+To use pretty print visitor, use the command:
+java -jar out\artifacts\psython2_jar\psython2.jar path/to/example/text
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import ast.expression.ExpressionList;
+import ast.visitor.PrettyPrintVisitor;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import parser.psythonASTLexer;
+import parser.psythonASTParser;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            final ANTLRFileStream afs = new ANTLRFileStream(args[0]);
+            final psythonASTLexer psyl = new psythonASTLexer(afs);
+            final CommonTokenStream cts = new CommonTokenStream(psyl);
+            final psythonASTParser psyp = new psythonASTParser(cts);
+
+            ExpressionList prg = psyp.program().result;
+
+            PrettyPrintVisitor pv = new PrettyPrintVisitor();
+            prg.accept(pv);
+            System.out.println(pv.result);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
